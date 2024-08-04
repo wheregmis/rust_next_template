64da4eb896e985d86ace1d60b23c320796a4a8ba
Sabin Regmi <get2sabin@gmail.com>
2024-08-03 22:10:02 -0400

    Using Cargo Watch

--- justfile
+++ justfile
@@ -1,6 +1,6 @@
 ui:
     cd client && npm run dev
 backend:
-    cargo run
+    cargo watch -x 'run'
 types:
     typester --input=./src/types.rs --output=./client/src/types.d.ts
--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn main() {
         .layer(cors);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("listening on {}", addr);
+    println!("Backend listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
